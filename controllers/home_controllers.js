36344bbc7d6b9bc5389e9010db058d605fe4183f
SadkiratSingh <sadkiratsingh@yahoo.com>
2021-04-14 18:33:20 +0530

    DATES AND DEADLINES HANDLED HURRAAYYY!!!!

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,12 +1,64 @@
 const ctgModel = require('../modules/category').ctgModel
+const oneDay = 24*60*60*1000;
 
+//utility functions
+function calcDaysLeftUtil(last,current){
+    let milliLast = last.getTime();
+    let milliCurrent = current.getTime();
+    let milliLeft = milliLast - milliCurrent;
+    return Math.round(milliLeft/oneDay);
+}
+
+function convertIST(utcDate){
+    let localDateString = utcDate.toLocaleString();
+    let date = new Date(localDateString);
+    return date;
+}
+
+function calcDaysLeft(lastDate){
+    let daysLeft = calcDaysLeftUtil(convertIST(lastDate),new Date());
+    return daysLeft;
+}
+
+function pastDeadline(lastDate){
+    let a = new Date().getTime();
+    let b = convertIST(lastDate).getTime();
+    return a > b;
+}
+
+function calcLastDate(lastDate){
+    let localDateString = lastDate.toLocaleString();
+    return localDateString.split(",")[0];
+}
+
+// for what to display
 function home(req,res){
-    ctgModel.find({},function(err,categories){
+    let modelQuery = ctgModel.find().lean();
+    modelQuery.exec(function(err,categories){
         if(err) return console.log(err);
+
+        let allTasks=[];
+        categories.forEach((doc)=>{
+          allTasks=allTasks.concat(doc.tasks);
+        });
+
+        allTasks.forEach((task)=>{
+            task.lastDate = calcLastDate(task.deadline);
+            task.daysLeft = calcDaysLeft(task.deadline);
+            if(task.daysLeft <= 7){
+                task.warningMsg = "We are approaching the deadline";
+            }
+            if(pastDeadline(task.deadline)){
+                task.expireMsg = "We are past the deadline";
+            }
+        });
+
+        console.log(allTasks);
         return res.render('home',{
             'categories':categories,
+            'alltasks': allTasks
         });
-    })
+    });
 }
 
 module.exports={
--- controllers/task_controller.js
+++ controllers/task_controller.js
@@ -5,14 +5,16 @@ function create(req,res){
     let task = req.body.name;
     let deadline = req.body.deadline;
     let category = req.body.category;
+
     ctgModel.findOne({name:category},function(err,ctgDoc){
         if(err) return console.log(err);
+        
         ctgDoc.tasks.push({name:task,deadline:deadline});
         ctgDoc.save(function(err){
             if(err) return console.log(err)
             console.log("Success!!");
-        })
-    })
+        });
+    });
 }
 
 module.exports={
